009ee22554b419e5bc473d981f6eb36888cbcdc6
Paulo Roberto <pauloroberto@MacBook-Pro-de-Paulo.local>
2020-09-04 20:49:16 -0300

    Correcao

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     );
 
     if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
         return res.status(200).send({});
     }
 
